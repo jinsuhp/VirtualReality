0b34f3c149f8e44f52d44e7b3e270db4b5040018
jinsuhp <jinsuhp@nycstudents.net>
2025-10-17 16:29:47 +0000

    Practical 2.1

--- VRPractical2_2/script.js
+++ VRPractical2_2/script.js
@@ -1,5 +1,50 @@
+let scene;
+let car;
+let poke;
+let rocket;
+let dude;
+let sun;
+
 
 window.addEventListener("DOMContentLoaded",function() {
+    scene = document.querySelector("a-scene");
+    car = document.querySelector("#car");
+    poke = document.querySelector("#pokemonball");
+    rocket = document.querySelector("#rocket");
+    dude = document.querySelector("#dude");
+    sun = document.querySelector("#sun");
 
+    car.x = 0;
+    car.xd = 0.01;
+
+    poke.x = 0;
+    poke.xs = 1;
     
+    rocket.y = 0;
+    rocket.ys = 0.05;
+
+    dude.s = 0;
+    dude.sg = 0.01;
+
+    sun.s = 0;
+    sun.sg = 0.005;
+    loop();
 })
+
+function loop(){
+    car.x += car.xd;
+    car.setAttribute("position",{x:-car.x, y:0, z:0});
+
+    poke.x += poke.xs;
+    poke.setAttribute("rotation",{x:poke.x, y:0, z:0});
+
+    rocket.y += rocket.ys;
+    rocket.setAttribute("position", {x:0, y:rocket.y, z:0});
+
+    dude.s += dude.sg;
+    dude.setAttribute("scale", {x:dude.s,y:dude.s,z:dude.s});
+
+    sun.s += sun.sg;
+    sun.setAttribute("opacity", sun.s)
+  window.requestAnimationFrame(loop);
+}
