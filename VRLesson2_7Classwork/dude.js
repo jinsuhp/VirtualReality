11fb98881410a13d83989d29287b580b03f40845
jinsuhp <jinsuhp@nycstudents.net>
2025-11-05 04:16:15 +0000

    2.7

--- VRLesson2_7Classwork/dude.js
+++ VRLesson2_7Classwork/dude.js
@@ -4,17 +4,22 @@ class Dude{
     this.y = y;
     this.z = z;
     //Challenge 1: Add delta variables in order to make the dude walk forward on the z axis and jump up and down on the y axis.
-    this.z -= this.dz;
-    this.y += this.dy;
+    this.dy = 0.01;
+    this.dz = 0.01;
+
+
     this.obj = dudeTemplate.cloneNode(true);
     this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
     scene.append(this.obj);
   }
   jump(){
     //Challenge 2: Add the accumulators to modify the y and z variables
-
+    this.z -= this.dz;
+    this.y += this.dy;
     //Challenge 3: Add the decision in order to alternate the delta on the y axis when the dude reaches less then 1 or greater than 2
-
+    if (this.y >= 2 || this.y <= 1) {
+      this.dy = -this.dy;
+    }
     this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
   }
 }
--- VRLesson2_7Classwork/script.js
+++ VRLesson2_7Classwork/script.js
@@ -1,16 +1,25 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene, dudeTemplate;
+let scene, dudeTemplate, dudes = [];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   dudeTemplate = document.querySelector("#dudeTemplate");
 
   //Challenge 4: Create an array of dudes at random locations.
-  
+  for(let i =0; i < 10; i++){
+    let x = rnd(-40,40);
+    let z = rnd(-40,40);
+    let y = rnd(1,2);
+
+    let d = new Dude(x,y,z);
+    dudes.push(d);
+  }
   loop();  
 })
 function loop(){
   //Challenge 5: Make all the dudes jump.
-  
+  for(let d of dudes){
+    d.jump();
+  }
   window.requestAnimationFrame( loop );
 }
