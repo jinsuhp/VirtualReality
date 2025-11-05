b14aeaedd37b1a4463ce80a57afab2ef38db94fd
jinsuhp <jinsuhp@nycstudents.net>
2025-11-05 03:45:17 +0000

    2.6

--- VRLesson2_6Classwork/mech.js
+++ VRLesson2_6Classwork/mech.js
@@ -1,9 +1,11 @@
 class Mech{
     constructor(x,y,z){
         this.x;
-        this,y;
+        this.y;
         this.z;
         this.obj = mechs.cloneNode(true);
+
+        this.obj.setAttribute("position",{x:this.x,y:this.y,z:this.z});
 
         scene.append(this.obj)
     }
--- VRLesson2_6Classwork/script.js
+++ VRLesson2_6Classwork/script.js
@@ -1,12 +1,23 @@
-let scene, mech;
+let scene, mechs = [], mechTemplate;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   
   mech = document.getElementById("mech-1");
+  
+  for(let i = 0; i < 25; i++){
+    x = rnd(-100,100);
+    z = rnd(-100,100);
+
+    let m = new Mech(x,0,z);
+    mechs.push(m);
+  }
+  
  
   loop();
 })
+
+
 function loop(){
 
 
